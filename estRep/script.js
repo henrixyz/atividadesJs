0fc516d423bfd53ec3e3beca9327b66e1cf7cece
henrique <115552341+henrixyz@users.noreply.github.com>
2023-05-23 19:32:06 -0300

    Add files via upload

---
+++ estRep/script.js
@@ -0,0 +1,26 @@
+for (let i = 0; i < array.length; i++) {//inicia contador; tamanho do vetor; incremento
+    const variavel = array[i];
+}
+////////////////////////
+
+while (condição) {
+    
+}
+///////////////////////
+
+do {//condicao depois
+    
+} while (condição);
+
+///////////////////////
+
+for (const chave in objeto) {//pra objeto
+
+}
+
+////////////////////////
+
+for (const iterador of objeto) {//pra vetor
+    
+}
+//////////////////////////
